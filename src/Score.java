051f7c50c74768ba453f70117e6c1603b0257d99
Kamil Przebieracz <dmjs23@gmail.com>
2022-08-16 16:50:06 +0200

    Update Score.java
    remove 2nd box

--- src/Score.java
+++ src/Score.java
@@ -30,6 +30,5 @@ public class Score extends Rectangle {
         g.drawLine(0, 5, GAME_WIDTH, 5);// upper line
         g.drawLine(0, GAME_HEIGHT - 5, GAME_WIDTH, GAME_HEIGHT - 5);// bottom line
         g.drawRect(GAME_WIDTH / 2 - 100, 5, 200, 70); // draw box for score
-        g.drawRect(GAME_WIDTH / 2 - 105, 0, 210, 80); // draw box 2 for score
     }
 }
